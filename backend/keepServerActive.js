875f19d49569a824bf8d4cf339e628b81f4bbd7d
Patrick O'Brien <obrienp2321@gmail.com>
2023-01-24 08:15:00 +0100

    backend update

---
+++ backend/keepServerActive.js
@@ -0,0 +1,39 @@
+// const axios = require('axios');
+const schedule = require("node-schedule");
+
+function keepServerActive() {
+  // Schedule the API call to run at 9:30 am every day
+  schedule.scheduleJob("*/1 * * * *", () => {
+    // Make the API call
+    // axios.get('https://example.com/api/keep-alive')
+    //   .then(response => {
+    //     console.log('Server is active:', response.data);
+    //   })
+    //   .catch(error => {
+    //     console.log('Error:', error);
+    //   });
+    try {
+      let data = {
+        name: "KEEPING SERVER ALIVE 13 MINUTES",
+        message: "KEEPING SERVER ALIVE 13 MINUTES",
+      };
+      // setBool(true);
+      const res = axios.post(
+        "https://hpnotepad-test-api.onrender.com/contact",
+        data
+      );
+      if (data.message.length === 0) {
+        console.log(res.data.message);
+        // setBool(false);
+      } else if (res.status === 200) {
+        console.log(res.data.message);
+        // setBool(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("Server is active");
+  });
+}
+
+module.exports = keepServerActive;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const keepServerActive = require('./keepServerActive');
 const contactRoute = require("./route/contactRoute");
 
 const app = express();
+
+keepServerActive();
 
 app.use(
   "/",
