6d25fb9dc8898cf74ef9bbeb1485f70f5b48626d
Patrick O'Brien <obrienp2321@gmail.com>
2023-01-24 08:46:20 +0100

    node-cron

--- backend/keepServerActive.js
+++ backend/keepServerActive.js
@@ -1,21 +1,41 @@
-const axios = require('axios');
+const axios = require("axios");
 const schedule = require("node-schedule");
+const nodeCron = require("node-cron");
 
 function keepServerActive() {
   // Schedule the API call to run at 9:30 am every day
-  schedule.scheduleJob("*/13 * * * *", () => {
-    // Make the API call
-    // axios.get('https://example.com/api/keep-alive')
-    //   .then(response => {
-    //     console.log('Server is active:', response.data);
-    //   })
-    //   .catch(error => {
-    //     console.log('Error:', error);
-    //   });
+  //   schedule.scheduleJob("*/13 * * * *", () => {
+
+  //     try {
+  //       let data = {
+  //         name: "KEEPING SERVER ALIVE 13 MINUTES",
+  //         message: "KEEPING SERVER ALIVE 13 MINUTES",
+  //       };
+  //       // setBool(true);
+  //       const res = axios.post(
+  //         "https://hpnotepad-test-api.onrender.com/contact",
+  //         data
+  //       );
+  //       console.log("res", res);
+  //       if (data.message.length === 0) {
+  //         console.log(res.data.message);
+  //         // setBool(false);
+  //       } else if (res.status === 200) {
+  //         console.log(res.data.message);
+  //         // setBool(false);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //     console.log("Server is active");
+  //   });
+
+  const job = nodeCron.schedule("*1 * * * *", function jobYouNeedToExecute() {
+    // Do whatever you want in here. Send email, Make  database backup or download data.
     try {
       let data = {
-        name: "KEEPING SERVER ALIVE 13 MINUTES",
-        message: "KEEPING SERVER ALIVE 13 MINUTES",
+        name: "cron KEEPING SERVER ALIVE 13 MINUTES",
+        message: "cron KEEPING SERVER ALIVE 13 MINUTES",
       };
       // setBool(true);
       const res = axios.post(
@@ -33,8 +53,9 @@ function keepServerActive() {
     } catch (error) {
       console.log(error);
     }
-    console.log("Server is active");
+    console.log(new Date().toLocaleString());
   });
+  job();
 }
 
 module.exports = keepServerActive;
