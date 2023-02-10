5a2589b508d3e98a7a428d073748a09f5b9b2212
Patrick O'Brien <obrienp2321@gmail.com>
2023-02-10 22:20:27 +0100

    log 14 minutes

--- frontend/src/reducers/dashboardReducers.js
+++ frontend/src/reducers/dashboardReducers.js
@@ -9,7 +9,19 @@ export const dashboardReducer = (state = {}, action) => {
     case FETCH_DATA_REQUEST:
       return { loading: true };
     case FETCH_DATA_SUCCESS:
-      return { loading: false, today: action.payload };
+      return {
+        loading: false,
+        today: action.payload,
+        yesterday: action.payload,
+        twoDaysAgo: action.payload,
+        threeDaysAgo: action.payload,
+        fourDaysAgo: action.payload,
+        fiveDaysAgo: action.payload,
+        sixDaysAgo: action.payload,
+        sevenDaysAgo: action.payload,
+        eightDaysAgo: action.payload,
+        nineDaysAgo: action.payload,
+      };
     case FETCH_DATA_FAILURE:
       return { loading: false, error: action.payload };
     default:
